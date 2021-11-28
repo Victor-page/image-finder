27fb51da00befca6dbc71b637510addcc5db37e3
Victor-page <victor.shevchuk.96@gmail.com>
2021-11-28 20:57:52 +0300

    added svg icon

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,18 +1,22 @@
 import classes from './Searchbar.module.css';
 
+import { ReactComponent as SearchIcon } from 'icons/search.svg';
+
 const Searchbar = ({ onSubmit }) => {
   return (
-    <header class={classes.searchbar}>
-      <form class={classes['search-form']} onSubmit={onSubmit}>
-        <button type="submit" class={classes['search-form-button']}>
-          <span class="button-label">Search</span>
+    <header className={classes.searchbar}>
+      <form className={classes['search-form']} onSubmit={onSubmit}>
+        <button type="submit" className={classes['search-form-button']}>
+          <span className="button-label">
+            <SearchIcon width="24px" height="24px" />
+          </span>
         </button>
 
         <input
           className={classes['search-form-input']}
           type="text"
-          autocomplete="off"
-          autofocus
+          autoComplete="off"
+          autoFocus
           placeholder="Search images and photos"
         />
       </form>
